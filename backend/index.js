aee1062298f90bfeb89872cea97f66428f04f5c3
Yury Slepianok <slepianok@gmail.com>
2019-12-05 00:50:13 +0300

    Initial. Ready worker and half-ready api

---
+++ backend/index.js
@@ -0,0 +1,33 @@
+const keys = require('./keys');
+
+// Express app setup
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+// Postgres client setup
+const { Pool } = require('pg');
+const pgClient = new Pool({
+    host: keys.pgHost,
+    port: keys.pgPort,
+    user: keys.pgUser,
+    password: keys.pgPassword,
+    database: keys.pgDatabase
+});
+
+pgClient.on('error', () => console.log('Something went wrong with PG'));
+pgClient.query('CREATE TABLE IF NOT EXISTS values (number INT)')
+    .catch((err) => console.log(err));
+
+// Redis client setup
+const redis = require('redis');
+const redisClient = redis.createClient({
+    host: keys.redisHost,
+    port: keys.redisPort,
+    retry_strategy: () => 1000
+});
+const redisPublisher = redisClient.duplicate();
