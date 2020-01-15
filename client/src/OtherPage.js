44c53d9a7475da8d56c45f7d50e3dc8575f96907
Yury Slepianok <slepianok@gmail.com>
2020-01-16 00:23:52 +0300

    typo fix

--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -1,11 +1,11 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default () => {
     return (
         <div>
             I'm some other page!
-            <link to="/">Go back home</link>
+            <Link to="/">Go back home</Link>
         </div>
     );
 }
